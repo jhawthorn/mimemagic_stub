2cf53cb84b48658b454c7d2f9cbe95dcb0b0dac8
John Hawthorn <john@hawthorn.email>
2021-03-24 14:24:11 -0700

    Add stubbed API

--- lib/mimemagic.rb
+++ lib/mimemagic.rb
@@ -1,4 +1,26 @@
 # frozen_string_literal: true
 
-module Mimemagic
+module MimeMagic
+  TYPES = {}
+
+  def initialize(type)
+  end
+
+  def extensions
+    []
+  end
+
+  class << self
+    def by_magic(*)
+    end
+
+    def by_extension(*)
+    end
+
+    def by_path(*)
+    end
+
+    def add(*)
+    end
+  end
 end
